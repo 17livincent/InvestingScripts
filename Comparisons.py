e6aa7356441952afe2683a61905d87d2f99d3c4d
Vincent Li <li.vincent0@gmail.com>
2026-05-14 22:42:47 -0700

    Additional edits to Comparison

--- Comparisons.py
+++ Comparisons.py
@@ -7,7 +7,21 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import PercentFormatter
 from datetime import datetime, timedelta
 
-tickers = ['PLTR', 'TEAM', 'APH', 'F', 'SNDK', 'BROS']
+tickers = [
+    'PLTR',
+    # 'TEAM',
+    'APH',
+    # 'F',
+    'SNDK',
+    # 'BROS',
+    'MU',
+    # 'NVDA',
+    'AVGO',
+    # 'META',
+    'ETN',
+    'V',
+    'MA'
+    ]
 
 graphs = [{'x': 'Date', 'y': 'TTM_ROIC'},
           {'x': 'Date', 'y': 'RevenueGrowth_YoY'},
@@ -20,29 +34,39 @@ comparison_rows = []
 now = pd.to_datetime(datetime.now())
 
 for ticker in tickers:
-    df_calculated = read_saved_fundamentals(ticker)
+    df_calculated = pd.DataFrame()
+    try:
+        df_calculated = read_saved_fundamentals(ticker)
+        comparison_rows.append(get_latest_metrics(df_calculated, ticker))
+    except FileNotFoundError as e:
+        print(e)
+        print("WARNING: no data for {}.".format(ticker))
     df_calculated_all[ticker] = df_calculated
-    comparison_rows.append(get_latest_metrics(df_calculated, ticker))
 
 df_comparison = pd.DataFrame(comparison_rows)
 df_comparison = df_comparison.sort_values(by='TTM_ROIC', ascending=False)
 print(df_comparison)
 
+top_ttm_roic = df_comparison.iloc[0]['Ticker']
+
 fig, ax = plt.subplots(2, 2, figsize=(16, 8))
 
 for ticker in tickers:
     ticker_calculated = df_calculated_all[ticker]
-    since_2020_calculated = ticker_calculated[now - ticker_calculated['Date'] < timedelta(weeks=52*6)]
+    if not df_calculated_all[ticker].empty:
+        since_2020_calculated = ticker_calculated[now - ticker_calculated['Date'] < timedelta(weeks=52*6)]
 
-    for graph_num, graph_x_y in enumerate(graphs):
-        row = int(graph_num / 2)
-        col = graph_num % 2
-        # Graph TTM_ROIC over time
-        ax[row,col].set_title(graph_x_y['y'])
-        ax[row,col].plot(since_2020_calculated[graph_x_y['x']], since_2020_calculated[graph_x_y['y']], label=ticker)
-        ax[row,col].legend()
-        ax[row,col].grid(True, alpha=0.3)
-        ax[row,col].yaxis.set_major_formatter(PercentFormatter(1.0))
+        for graph_num, graph_x_y in enumerate(graphs):
+            row, col = divmod(graph_num, 2)
+            # Graph TTM_ROIC over time
+            ax[row,col].set_title(graph_x_y['y'])
+            ax[row,col].plot(since_2020_calculated[graph_x_y['x']],
+                            since_2020_calculated[graph_x_y['y']],
+                            label=ticker,
+                            linewidth=(3 if ticker == top_ttm_roic else 1.5))
+            ax[row,col].legend(loc='center left', bbox_to_anchor=(1, 0.5))
+            ax[row,col].grid(True, alpha=0.3)
+            ax[row,col].yaxis.set_major_formatter(PercentFormatter(1.0))
 
 fig.suptitle('Comparisons', fontsize=24)
 fig.tight_layout()
