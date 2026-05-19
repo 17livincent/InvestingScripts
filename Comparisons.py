5e30e0643d9d5692b5c02c6d119a6de0bf1843d2
Vincent Li <li.vincent0@gmail.com>
2026-05-18 22:44:39 -0700

    Graph PE_TTM, EV_EBIT, EV_FCF

--- Comparisons.py
+++ Comparisons.py
@@ -20,18 +20,34 @@ tickers = [
     # 'NVDA',
     # 'AVGO',
     # 'META',
-    'V',
-    'MA',
+    # 'V',
+    # 'MA',
     # 'TMUS',
     # 'VZ',
-    'FDX',
+    # 'FDX',
     'CRWD'
     ]
 
-graphs = [{'x': 'Date', 'y': 'TTM_ROIC'},
-          {'x': 'Date', 'y': 'RevenueGrowth_YoY'},
-          {'x': 'Date', 'y': 'TTM_OperatingMargin'},
-          {'x': 'Date', 'y': 'TTM_FCFMargin'}]
+FIGURE_ROWS = 4
+FIGURE_COLS = 2
+OPERATIONAL_TIME_FRAME_WEEKS = 52*6
+VALUATION_TIME_FRAME_WEEKS = 52*2
+
+time_frames = {'TTM_ROIC': OPERATIONAL_TIME_FRAME_WEEKS,
+               'RevenueGrowth_YoY': OPERATIONAL_TIME_FRAME_WEEKS,
+               'TTM_OperatingMargin': OPERATIONAL_TIME_FRAME_WEEKS,
+               'TTM_FCFMargin': OPERATIONAL_TIME_FRAME_WEEKS,
+               'PE_TTM': VALUATION_TIME_FRAME_WEEKS,
+               'EV_EBIT': VALUATION_TIME_FRAME_WEEKS,
+               'EV_FCF': VALUATION_TIME_FRAME_WEEKS}
+
+graphs = [{'x': 'Date', 'y': 'TTM_ROIC', 'percentFormat': True},
+          {'x': 'Date', 'y': 'RevenueGrowth_YoY', 'percentFormat': True},
+          {'x': 'Date', 'y': 'TTM_OperatingMargin', 'percentFormat': True},
+          {'x': 'Date', 'y': 'TTM_FCFMargin', 'percentFormat': True},
+          {'x': 'Date', 'y': 'PE_TTM', 'percentFormat': False},
+          {'x': 'Date', 'y': 'EV_EBIT', 'percentFormat': False},
+          {'x': 'Date', 'y': 'EV_FCF', 'percentFormat': False}]
 
 df_calculated_all = {}
 comparison_rows = []
@@ -61,24 +77,29 @@ print(df_comparison)
 top_ttm_roic = df_comparison.iloc[0]['Ticker']
 
 plt.style.use('dark_background')
-fig, ax = plt.subplots(2, 2, figsize=(16, 8))
+fig, ax = plt.subplots(FIGURE_ROWS, FIGURE_COLS, figsize=(18, 12))
 
 for ticker in tickers:
-    ticker_calculated = df_calculated_all[ticker]
-    if not df_calculated_all[ticker].empty:
-        since_2020_calculated = ticker_calculated[now - ticker_calculated['Date'] < timedelta(weeks=52*6)]
 
         for graph_num, graph_x_y in enumerate(graphs):
-            row, col = divmod(graph_num, 2)
-            # Graph TTM_ROIC over time
-            ax[row,col].set_title(graph_x_y['y'])
-            ax[row,col].plot(since_2020_calculated[graph_x_y['x']],
-                            since_2020_calculated[graph_x_y['y']],
-                            label=ticker,
-                            linewidth=(3 if ticker == top_ttm_roic else 1.5))
-            ax[row,col].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-            ax[row,col].grid(True, alpha=0.3)
-            ax[row,col].yaxis.set_major_formatter(PercentFormatter(1.0))
+            ticker_calculated = df_calculated_all[ticker]
+            since_calculated = ticker_calculated[now - ticker_calculated['Date'] < timedelta(weeks=time_frames[graph_x_y['y']])]
+
+            if not df_calculated_all[ticker].empty:
+
+                row, col = divmod(graph_num, FIGURE_COLS)
+                ax[row,col].set_title(graph_x_y['y'])
+                try:
+                    ax[row,col].plot(since_calculated[graph_x_y['x']],
+                                    since_calculated[graph_x_y['y']],
+                                    label=ticker,
+                                    linewidth=(3 if ticker == top_ttm_roic else 1.5))
+                    ax[row,col].legend(loc='center left', fontsize=8, bbox_to_anchor=(1, 0.5))
+                except KeyError as e:
+                    print("WARNING key {} missing for {}.".format(graph_x_y['y'], ticker))
+                ax[row,col].grid(True, alpha=0.3)
+                if graph_x_y['percentFormat'] == True:
+                    ax[row,col].yaxis.set_major_formatter(PercentFormatter(1.0))
 
 fig.suptitle('Comparisons', fontsize=24)
 fig.tight_layout()
