57dcca0c80e3b0f546e4d2be442ddcb07877dd2e
Vincent Li <li.vincent0@gmail.com>
2026-06-10 22:02:06 -0700

    Add get_index_time_series_daily()

--- IndexData.py
+++ IndexData.py
@@ -5,6 +5,8 @@
 from RequestAndSave import request_index_catalog, get_api_key
 import json
 import requests
+from datetime import datetime, timezone
+import pandas as pd
 
 def request_index_list():
     data_json = request_index_catalog()
@@ -25,11 +27,32 @@ def get_index_list():
 
     return index_list_dict
 
-def get_index_time_series_daily(index_symbol):
+def get_index_time_series_daily(index_symbol, minimum_date:datetime):
     url = 'https://www.alphavantage.co/query?function={}&symbol={}&interval={}&apikey={}'.format('INDEX_DATA',
                                                                                                  index_symbol,
                                                                                                  'daily',
                                                                                                  get_api_key())
     r = requests.get(url)
-    data = r.json()
-    return data
+    data_json = r.json()
+    df_index_data = pd.DataFrame()
+
+    if 'data' in list(data_json):
+        for data in data_json['data']:
+            data['date'] = datetime.strptime(data['date'], '%Y-%m-%d').astimezone(timezone.utc)
+
+        df_index_data['date'] = [data['date'] for data in data_json['data']
+                                 if data['date'] >= minimum_date]
+
+        df_index_data['open'] = [data['open'] for data in data_json['data']
+                                 if data['date'] >= minimum_date]
+
+        df_index_data['high'] = [data['high'] for data in data_json['data']
+                                 if data['date'] >= minimum_date]
+
+        df_index_data['low'] = [data['low'] for data in data_json['data']
+                                if data['date'] >= minimum_date]
+
+        df_index_data['close'] = [data['close'] for data in data_json['data']
+                                  if data['date'] >= minimum_date]
+
+    return df_index_data
